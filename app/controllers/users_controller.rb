b96f911a56599c9bdc059950ec7869eb86c11f7c
HomareYamamoto <159757552+HomareYamamoto@users.noreply.github.com>
2024-03-16 13:05:48 +0000

    [finish]4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user=User.find(params[:id])
   end
 
+  def update
+    @user=User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
 
+  private
 
-
-  # private
-
-  # def post_image_params
-  #   params.require(:post_image).permit(:shop_name, :image, :caption)
-  # end
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 
 
 
